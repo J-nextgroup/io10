e6894cc50c269c69b7508894da6b02f53884c37d
Synco <synco@Syncos-MacBook-Pro.local>
2021-07-18 22:10:33 +0200

    Piccalilly Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import { PiccalillyComponent } from './piccalilly/piccalilly.component'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule
-  ],
+  declarations: [AppComponent, PiccalillyComponent],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/piccalilly/piccalilly.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-piccalilly',
+  templateUrl: './piccalilly.component.html',
+  styleUrls: ['./piccalilly.component.scss']
+})
+export class PiccalillyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
