50e28f935c754957a51ff978013cc9cee64f1e5d
Synco <synco@Syncos-MacBook-Pro.local>
2021-08-03 22:36:34 +0200

    Added correct links

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-header',
@@ -9,6 +9,4 @@ export class HeaderComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
-
-  onCatering() {}
 }
