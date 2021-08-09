56b47604b3a0b6aa2086cb66fd4e5c4813880477
Synco <synco@Syncos-MacBook-Pro.local>
2021-08-10 00:45:54 +0200

    Updated styling and added hamburger menu

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -6,7 +6,39 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  collapsed = true
+  menuitems: { url: string; text: string }[] = [
+    {
+      url: '/#themes',
+      text: 'io/10',
+    },
+    {
+      url: '/#themes',
+      text: "thema's/sprekers",
+    },
+    {
+      url: '/#timetable',
+      text: 'timetable',
+    },
+    {
+      url: '/#catering',
+      text: 'catering',
+    },
+    {
+      url: '/#parking',
+      text: 'parkeren',
+    },
+  ]
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  toggleCollapsed(): void {
+    this.collapsed = !this.collapsed
+  }
+
+  selectMenu() {
+    this.collapsed = true
+  }
 }
